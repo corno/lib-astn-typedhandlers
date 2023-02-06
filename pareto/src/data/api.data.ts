bf22c874a5407f514efc689a2ef20501e4c4f9cf
corno <corno.astn.a@schraverus.com>
2023-02-06 17:37:08 +0100

    .

---
+++ pareto/src/data/api.data.ts
@@ -0,0 +1,35 @@
+import * as pr from 'pareto-core-raw'
+import {
+    reference,
+    string,
+    typeReference,
+    interfaceReference,
+    method, dictionary, group, member, taggedUnion, types, func, data
+} from "lib-pareto-typescript-project/dist/submodules/glossary/shorthands.p"
+
+import { algorithm, constructor, definitionReference, } from "lib-pareto-typescript-project/dist/submodules/moduleDefinition/shorthands.p"
+
+import * as mmoduleDefinition from "lib-pareto-typescript-project/dist/submodules/moduleDefinition"
+
+const d = pr.wrapRawDictionary
+
+export const $: mmoduleDefinition.TModuleDefinition = {
+    'glossary': {
+        'imports': d({
+            "common": "glo-pareto-common"
+        }),
+        'parameters': d({}),
+        'templates': d({}),
+        'types': types({
+        }),
+        'interfaces': d({
+        }),
+        'functions': d({ }),
+    },
+    'api': {
+        'imports': d({
+            "common": "glo-pareto-common",
+        }),
+        'algorithms': d({ })
+    },
+}
---
+++ pareto/src/data/project.data.ts
@@ -0,0 +1,32 @@
+import * as pr from 'pareto-core-raw'
+
+import * as mproject from "lib-pareto-typescript-project/dist/submodules/project"
+
+const d = pr.wrapRawDictionary
+
+import { $ as api } from "./api.data"
+
+export const $: mproject.TProject = {
+    'author': "Corno",
+    'description': "makes it straightforward to generate properly indented source code files",
+    'license': "ISC",
+
+    'pubdependencies': d({
+        "glo-pareto-common": {},
+        "lib-pareto-filesystem": {},
+        //"res-pareto-filesystem": {},
+        "res-pareto-tostring": {},
+    }),
+    'type': ['library', {
+        'main': {
+            'definition': api,
+        },
+        'submodules': d({
+        }),
+        'test': {
+            'dependencies': d({
+                "res-pareto-tostring": {},
+            }),
+        }
+    }],
+}
